e3cfa9e4c49ffde0869032f60dffe7e9968dd101
cnkapper <145476033+cnkapper@users.noreply.github.com>
2023-12-14 11:33:06 -0500

    Delete createfile1.R, unnecessary file

--- createfile1.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-df <- readr::read_csv("~/work/medstudentdata.csv")
-
-
-plot <- ggplot(data = df, mapping = aes(group = year, y = mbi_ex)) + geom_boxplot() + 
-  labs(x = "Year", y = "Exhaustion Score", 
-       title = "Distribution of Exhaustion Scores for Each Year")
-
-ggsave(filename = "kapper_figure1.png", plot)
